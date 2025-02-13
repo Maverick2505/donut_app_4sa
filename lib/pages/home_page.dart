66e40b0e661af42ed58718a8e1116269e68c4d3c
Hector Illescas <hector.gpillescas@gmail.com>
2025-02-13 13:32:45 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,11 +13,46 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor:Colors.transparent,
-        leading: Icon(Icons.menu,
-        color:Colors.grey[800],),
+        leading: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Icon(Icons.menu,
+          color:Colors.grey[800],),
+        ),
       actions: [
-        Icon(Icons.person)]
+        Padding(
+          padding: const EdgeInsets.only(right: 24.0),
+          child: Icon(Icons.person),
+        )]
       ),
+      body: Column(
+        children: <Widget>[
+        //1 Texto principal(Text)
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Row(
+            children: [
+              Text("I want to ",style: TextStyle(
+                fontSize: 24,
+              )),
+              Text("Eat",style: TextStyle(
+                // Tamaño de letra
+                fontSize: 32,
+                //Negritas
+                fontWeight: FontWeight.bold,
+                //Subrayado
+                decoration: TextDecoration.underline,
+              ))
+            ],
+          ),
+        )
+
+        //2 Pestañas(TabBar)
+
+        // Contenido de pestañas(TabBarView)
+
+        //4 Carrito (Car)
+        ],
+      )
     );
   }
 }
